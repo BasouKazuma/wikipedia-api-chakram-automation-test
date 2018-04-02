f31e63dfba6161c2678a2af9007db3cfc17f8c12
Abbas Jaffery <abbas@jobrapp.com>
2018-04-02 16:49:43 -0700

    Adds a file for handling global hooks

---
+++ tests/setupHooks.js
@@ -0,0 +1,16 @@
+
+before(function() {
+  // runs before all tests in every file are completed
+})
+
+after(function() {
+  // runs after all tests in every file are completed 
+})
+
+beforeEach(function() {
+  // runs before each test in every file
+})
+
+afterEach(function() {
+  // runs after each test in every file
+})
